dd302bfbf8ceddf3c0e8423eba311de410719c6c
avalon <asd95027@gmail.com>
2023-03-14 19:19:03 +0800

    1.增加勝負判斷 2.猜對時顯示猜了幾次

--- guess_game.py
+++ guess_game.py
@@ -5,15 +5,21 @@ import random
 
 
 x = random.randint(1, 50)
+print(x)
+win = False
 
 for i in range(5):
-    y = eval(input('請猜一個數字(1~50之間):'))
+    y = eval(input(f'[{i+1}/5]請猜一個數字(1~50之間):'))
     if x == y:
+        win = True
         break
 
     if x > y:
         print('猜大一點')
+
     else:
         print('猜小一點')
-
-print(f'遊戲結束!答案為:{x}')
+if win:
+    print(f'恭喜過關!共猜了{i+1}次')
+else:
+    print(f'遊戲結束!答案為:{x},共猜了{i+1}次')
