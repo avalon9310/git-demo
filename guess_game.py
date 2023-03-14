7076bd7bfdf20d894dd087475fda4eeac16918d2
avalon <asd95027@gmail.com>
2023-03-14 20:19:01 +0800

    新增區間提示,測試ok

--- guess_game.py
+++ guess_game.py
@@ -1,12 +1,14 @@
 import random
 
 # 1.增加勝負判斷
-# 2.提示區間
+# 2.猜對時顯示猜了幾次
+# 3.提示區間
 
 
 x = random.randint(1, 50)
 print(x)
 win = False
+start, end = 1, 50
 
 for i in range(5):
     y = eval(input(f'[{i+1}/5]請猜一個數字(1~50之間):'))
@@ -15,10 +17,13 @@ for i in range(5):
         break
 
     if x > y:
-        print('猜大一點')
+        start = y
+        print(f'猜大一點({start}~{end})')
 
     else:
-        print('猜小一點')
+        if y < end:
+            end = y
+        print(f'猜小一點({start}~{end})')
 if win:
     print(f'恭喜過關!共猜了{i+1}次')
 else:
